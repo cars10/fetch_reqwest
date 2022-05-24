df1f994031ef198250b49d0906eb88667f558d7d
Carsten König <carstenkoenig92@gmail.com>
2022-05-24 21:33:45 +0200

    add status_text

--- src/fetch/response.rs
+++ src/fetch/response.rs
@@ -5,14 +5,14 @@ use serde::Serialize;
 pub struct FetchResponse {
     pub ok: bool,
     pub status: u16,
+    pub status_text: String,
     pub text: String,
     pub headers: MultiMap<String, String>,
 }
 
 impl FetchResponse {
     pub async fn from_response(response: reqwest::Response) -> Result<Self, super::Error> {
-        let status = response.status().as_u16();
-        let ok = (200..=299).contains(&status);
+        let status = response.status();
         let mut headers = MultiMap::new();
         for (key, value) in response.headers() {
             headers.insert(key.as_str().to_string(), value.to_str()?.to_string());
@@ -20,8 +20,9 @@ impl FetchResponse {
         let text = response.text().await?;
 
         Ok(FetchResponse {
-            ok,
-            status,
+            ok: status.is_success(),
+            status: status.as_u16(),
+            status_text: status.canonical_reason().unwrap_or_default().to_string(),
             text,
             headers,
         })
