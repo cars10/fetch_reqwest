700889d19e098306d8dbde83318ddb198c308ff4
Carsten König <carstenkoenig92@gmail.com>
2024-03-10 18:51:12 +0100

    cleanup

--- src/fetch/mod.rs
+++ src/fetch/mod.rs
@@ -19,10 +19,10 @@ pub async fn fetch<U>(resource: U, init: Option<FetchOptions>) -> FetchResponseR
 where
     U: reqwest::IntoUrl + std::fmt::Debug,
 {
-    let client_builder = Client::builder()
+    let client = Client::builder()
         .danger_accept_invalid_certs(true)
-        .use_rustls_tls();
-    let client = client_builder.build()?;
+        .use_rustls_tls()
+        .build()?;
     let init = init.unwrap_or_default();
 
     let mut headers = HeaderMap::new();
@@ -35,26 +35,26 @@ where
         .headers(headers)
         .body(init.body.unwrap_or_default())
         .build()?;
-
+    
     let req_id = random::random_string(12).to_lowercase();
+    let request_url = request.url().to_string();
 
     info!(
-        "[{req_id}] {method} {url}",
-        method = request.method(),
-        url = request.url().to_string()
+        "[{}] {} {}",
+        req_id,
+        request.method(),
+        request_url
     );
     let response = client.execute(request).await?;
-    info!("[{req_id}] {status}", status = response.status());
+    info!("[{}] {}", req_id, response.status());
 
-    let fetch_response = FetchResponse::from_response(response).await;
+    let fetch_response = FetchResponse::from_response(response).await?;
 
-    if let Ok(fetch_response) = &fetch_response {
-        if fetch_response.ok {
-            debug!("[{req_id}] {text}", text = fetch_response.text);
-        } else {
-            error!("[{req_id}] {text}", text = fetch_response.text);
-        }
+    if fetch_response.ok {
+        debug!("[{}] {}", req_id, fetch_response.text);
+    } else {
+        error!("[{}] {}", req_id, fetch_response.text);
     }
 
-    fetch_response
+    Ok(fetch_response)
 }
